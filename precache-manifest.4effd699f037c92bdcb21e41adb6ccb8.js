6f8bf5e6e0c66fab7307ba7deb2ebd2785fadaa9
Marko Predovic <mpredovic@gmail.com>
2019-07-07 17:51:24 +0200

    Updates

---
+++ precache-manifest.4effd699f037c92bdcb21e41adb6ccb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11132ea88291f27db88df39d6390918",
+    "url": "/:markopredovic.github.io/skocko/index.html"
+  },
+  {
+    "revision": "c3a5302af84147dcda09",
+    "url": "/:markopredovic.github.io/skocko/static/css/main.1efc0539.chunk.css"
+  },
+  {
+    "revision": "b20f46be02b697a55c91",
+    "url": "/:markopredovic.github.io/skocko/static/js/2.b8beaffc.chunk.js"
+  },
+  {
+    "revision": "c3a5302af84147dcda09",
+    "url": "/:markopredovic.github.io/skocko/static/js/main.80e4aa86.chunk.js"
+  },
+  {
+    "revision": "b7e5293b37d97243f7eb",
+    "url": "/:markopredovic.github.io/skocko/static/js/runtime~main.20687d26.js"
+  }
+]);
